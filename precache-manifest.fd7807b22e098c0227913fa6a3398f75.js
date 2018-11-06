c731e128e1f795fa74e7d0453d334d653003c803
Kolsha <ovchinnikovnikola@mail.ru>
2018-11-06 18:19:26 +0700

    Updates

---
+++ precache-manifest.fd7807b22e098c0227913fa6a3398f75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a05ce87f2e67a10bef9",
+    "url": "/phonons-dispersion/static/js/runtime~main.5a05ce87.js"
+  },
+  {
+    "revision": "9d033d55ffaed1c1c1c2",
+    "url": "/phonons-dispersion/static/js/main.9d033d55.chunk.js"
+  },
+  {
+    "revision": "f559efdeb5ef21990fad",
+    "url": "/phonons-dispersion/static/js/1.f559efde.chunk.js"
+  },
+  {
+    "revision": "f559efdeb5ef21990fad",
+    "url": "/phonons-dispersion/static/css/1.2dd26a51.chunk.css"
+  },
+  {
+    "revision": "4239b0496eb9d90bfb25399c3ac8944c",
+    "url": "/phonons-dispersion/index.html"
+  }
+];
